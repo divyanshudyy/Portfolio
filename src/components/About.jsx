2c6b78d5f8aef9218aeff4a2dc460ff3229715ed
Divyanshu <divydivyanshu319@gmail.com>
2025-03-31 19:03:27 +0530

    About section done

---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+import ScrollReveal from "./ScrollReveal";
+
+export default function Hero() {
+  return (
+    <>
+      <section className="h-screen w-full flex justify-center items-center">
+        <ScrollReveal
+          baseOpacity={0}
+          enableBlur={true}
+          baseRotation={10}
+          blurStrength={20}
+          containerClassName="max-w-[1200px] mx-auto px-6 text-center opacity-75"
+        >
+          " I am a full-stack developer specializing in building impactful,
+          scalable, and efficient digital solutions. With expertise across
+          modern web technologies, I create future-ready applications that blend
+          performance, security, and seamless user experience . "
+        </ScrollReveal>
+      </section>
+    </>
+  );
+}
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,10 @@
 import BlurText from "./BlurText";
-import TrueFocus from "./TrueFocus";
 import ShinyText from "./ShinyText";
-import ScrollReveal from "./ScrollReveal";
 
 export default function Hero() {
   return (
     <>
-      <section className="h-screen w-full flex flex-col justify-center items-center">
+      <section className="h-screen w-full flex flex-col justify-center items-center font-extralight">
         <BlurText
           text="Transforming ideas into dynamic solutions"
           delay={100}
@@ -17,26 +15,11 @@ export default function Hero() {
         <br />
 
         <ShinyText
-          text="Full-Stack Web Developer | MERN Stack Enthusiast"
+          text="Full-Stack Developer | MERN Stack Enthusiast"
           disabled={false}
-          speed={3}
+          speed={2}
           className="custom-class text-2xl"
         />
-      </section>
-
-      <section className="h-screen w-full flex justify-center items-center">
-        <ScrollReveal
-          baseOpacity={0}
-          enableBlur={true}
-          baseRotation={10}
-          blurStrength={20}
-          containerClassName="max-w-[1200px] mx-auto px-6 text-center opacity-75"
-        >
-          " I am a full-stack developer specializing in building impactful,
-          scalable, and efficient digital solutions. With expertise across
-          modern web technologies, I create future-ready applications that blend
-          performance, security, and seamless user experience . "
-        </ScrollReveal>
       </section>
     </>
   );

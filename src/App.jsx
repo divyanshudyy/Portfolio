866c7cc70c27180aa6a2cec6e9faea4e1fb7d87a
Divyanshu <divydivyanshu319@gmail.com>
2025-04-08 22:35:49 +0530

    Skills Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Hero from "./components/Hero/Hero";
 import About from "./components/about/About";
 import Projects from "./components/project/Projects";
 import Blog from "./components/Blogs/Blog";
+import Skills from "./components/Skills/Skills";
 
 export default function App() {
   return (
     <>
       <Hero />
       <About />
+      <Skills />
       <Projects />
       <Blog />
     </>
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,109 @@
+import StarBorder from "./StarBorder";
+
+export default function Skills() {
+  return (
+    <>
+      <div className="h-screen w-full flex justify-center items-center">
+
+        <div className="h-1/2 w-2/3 flex justify-center items-center gap-10 flex-wrap">
+          <StarBorder
+            as="button"
+            className="custom-class w-40"
+            color="cyan"
+            speed="5s"
+          >
+            Node
+          </StarBorder>
+          <StarBorder
+            as="button"
+            className="custom-class w-40"
+            color="cyan"
+            speed="5s"
+          >
+            Express
+          </StarBorder>
+          <StarBorder
+            as="button"
+            className="custom-class w-40"
+            color="cyan"
+            speed="5s"
+          >
+            MongoDB
+          </StarBorder>
+          <StarBorder
+            as="button"
+            className="custom-class w-40"
+            color="cyan"
+            speed="5s"
+          >
+            React
+          </StarBorder>
+          <StarBorder
+            as="button"
+            className="custom-class w-40"
+            color="cyan"
+            speed="5s"
+          >
+            Node
+          </StarBorder>
+          <StarBorder
+            as="button"
+            className="custom-class w-40"
+            color="cyan"
+            speed="5s"
+          >
+            Express
+          </StarBorder>
+          <StarBorder
+            as="button"
+            className="custom-class w-40"
+            color="cyan"
+            speed="5s"
+          >
+            MongoDB
+          </StarBorder>
+          <StarBorder
+            as="button"
+            className="custom-class w-40"
+            color="cyan"
+            speed="5s"
+          >
+            React
+          </StarBorder>
+          <StarBorder
+            as="button"
+            className="custom-class w-40"
+            color="cyan"
+            speed="5s"
+          >
+            Node
+          </StarBorder>
+          <StarBorder
+            as="button"
+            className="custom-class w-40"
+            color="cyan"
+            speed="5s"
+          >
+            Express
+          </StarBorder>
+          <StarBorder
+            as="button"
+            className="custom-class w-40"
+            color="cyan"
+            speed="5s"
+          >
+            MongoDB
+          </StarBorder>
+          <StarBorder
+            as="button"
+            className="custom-class w-40"
+            color="cyan"
+            speed="5s"
+          >
+            React
+          </StarBorder>
+        </div>
+      </div>
+    </>
+  );
+}

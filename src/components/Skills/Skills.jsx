4bee082cfe637fbb4f97ba3b418f7914cd7afbf9
Divyanshu <divydivyanshu319@gmail.com>
2025-08-08 01:43:54 +0530

    changed design and layout

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,116 +1,35 @@
+import MagicBento from "./MagicBento";
 import StarBorder from "./StarBorder";
 import BlurText from "../Hero/BlurText";
 
 export default function Skills() {
   return (
-    <>
-      <section className="min-h-screen w-full flex flex-col justify-center items-center gap-8">
-        <BlurText
-          text="Skills"
-          delay={150}
-          animateBy="words"
-          direction="top"
-          className="text-4xl mb-8 text-white font-bold"
+    <section
+      id="skills"
+      className="h-screen w-full flex flex-col justify-center items-center px-4"
+    >
+      <BlurText
+        text="Tech Stack & Specializations"
+        delay={150}
+        animateBy="words"
+        direction="top"
+        className="text-4xl mb-8 text-white
+      font-bold"
+      />
+      <div className=" flex flex-col items-center justify-center ">
+        <MagicBento
+          textAutoHide={true}
+          enableStars={true}
+          enableSpotlight={true}
+          enableBorderGlow={true}
+          enableTilt={true}
+          enableMagnetism={true}
+          clickEffect={true}
+          spotlightRadius={200}
+          particleCount={20}
+          glowColor="132, 0, 255"
         />
-        <div className="h-1/2 w-4/5 flex justify-center items-center gap-5 flex-wrap">
-          <StarBorder
-            as="button"
-            className="custom-class w-40"
-            color="cyan"
-            speed="20s"
-          >
-            Node
-          </StarBorder>
-          <StarBorder
-            as="button"
-            className="custom-class w-40"
-            color="cyan"
-            speed="20s"
-          >
-            Express
-          </StarBorder>
-          <StarBorder
-            as="button"
-            className="custom-class w-40"
-            color="cyan"
-            speed="20s"
-          >
-            MongoDB
-          </StarBorder>
-          <StarBorder
-            as="button"
-            className="custom-class w-40"
-            color="cyan"
-            speed="20s"
-          >
-            React
-          </StarBorder>
-          <StarBorder
-            as="button"
-            className="custom-class w-40"
-            color="cyan"
-            speed="20s"
-          >
-            Node
-          </StarBorder>
-          <StarBorder
-            as="button"
-            className="custom-class w-40"
-            color="cyan"
-            speed="20s"
-          >
-            Express
-          </StarBorder>
-          <StarBorder
-            as="button"
-            className="custom-class w-40"
-            color="cyan"
-            speed="20s"
-          >
-            MongoDB
-          </StarBorder>
-          <StarBorder
-            as="button"
-            className="custom-class w-40"
-            color="cyan"
-            speed="20s"
-          >
-            React
-          </StarBorder>
-          <StarBorder
-            as="button"
-            className="custom-class w-40"
-            color="cyan"
-            speed="20s"
-          >
-            Node
-          </StarBorder>
-          <StarBorder
-            as="button"
-            className="custom-class w-40"
-            color="cyan"
-            speed="20s"
-          >
-            Express
-          </StarBorder>
-          <StarBorder
-            as="button"
-            className="custom-class w-40"
-            color="cyan"
-            speed="20s"
-          >
-            MongoDB
-          </StarBorder>
-          <StarBorder
-            as="button"
-            className="custom-class w-40"
-            color="cyan"
-            speed="20s"
-          >
-            React
-          </StarBorder>
-        </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 }
